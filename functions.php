f31fc0ab69c23ced05a1868528725e75bf0caaf0
Tasikul Islam <iamtasikul@gmail.com>
2020-05-17 12:22:13 +0530

    Open Registration Part 2

--- functions.php
+++ functions.php
@@ -100,3 +100,35 @@ function redirectSubsToFrontend()
   }
 }
 add_action('admin_init', 'redirectSubsToFrontend');
+
+function noSubsAdminBar()
+{
+  $ourCurrentUser = wp_get_current_user();
+
+  if (count($ourCurrentUser->roles) == 1 and $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+
+add_action('wp_loaded', 'noSubsAdminBar');
+
+function ourHeaderUrl()
+{
+  return esc_url(site_url('/'));
+}
+
+add_filter('login_headerurl', 'ourHeaderUrl');
+
+
+function ourLoginCSS()
+{
+  wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+}
+add_action('login_enqueue_scripts', 'ourLoginCSS');
+
+function ourLoginTitle()
+{
+  return get_bloginfo('name');
+}
+add_filter('login_headertext', 'ourLoginTitle');
